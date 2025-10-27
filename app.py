6ed77334abfb5fbc53ba76e152d29c1da75ed400
RinoTomić <62605541+rtomic20@users.noreply.github.com>
2025-10-27 16:25:37 +0100

    Commit bez env-a i Okruzenja

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 # ---------------------------------------------
 # Inicijalizacija
 # ---------------------------------------------
-load_dotenv("../.env")
+load_dotenv("env")
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 if not API_KEY:
